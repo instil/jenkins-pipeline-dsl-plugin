6d734580f3bfa8df4bc5a2147f64cae95182adbf
Chris van Es <cvanes@gmail.com>
2016-06-02 00:14:32 +0100

    A few minor tweaks to the gradle convenience task

--- src/main/resources/dsl/gradleBuild.groovy
+++ src/main/resources/dsl/gradleBuild.groovy
@@ -7,14 +7,14 @@ def call(body) {
     body.delegate = config
     body()
 
+    sendEmailNotificationOnFailure notifyCommitters: true
 
     tasks = config.tasks ?: "clean build"
     sh "./gradlew ${tasks}"
     archive includes: config.archiveFiles ?: "build/libs/*.jar"
-    publishJunitTestResults "build/test-results/**/TEST-*.xml"
-    publishCheckstyleResults pattern: "build/reports/checkstyle/main.xml"
-    publishFindbugsResults pattern: "build/reports/findbugs/main.xml"
-    publishPmdResults pattern: "build/reports/pmd/main.xml"
+    publishJunitTestResults junitResults ?: "build/test-results/**/TEST-*.xml"
+    publishCheckstyleResults pattern: checkstyleResults ?: "build/reports/checkstyle/main.xml"
+    publishFindbugsResults pattern: findbugsResults ?: "build/reports/findbugs/main.xml"
+    publishPmdResults pattern: pmdResults ?: "build/reports/pmd/main.xml"
     scanCodebaseForOpenTasks pattern: "src/main/**/*.java, src/main/**/*.kt"
-    sendEmailNotificationOnFailure notifyCommitters: true
 }
