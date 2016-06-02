4e4a49ca0bb9e42c560e67502c4612a68a69d76e
Chris van Es <cvanes@gmail.com>
2016-06-02 18:04:35 +0100

    Adding note regarding email notification DSL element

--- src/main/resources/dsl/sendEmailNotificationOnFailure.groovy
+++ src/main/resources/dsl/sendEmailNotificationOnFailure.groovy
@@ -1,5 +1,11 @@
 package dsl
 
+/*
+ * This currently does not allow sending emails to committers due a Jenkins pipeline bug.
+ * https://issues.jenkins-ci.org/browse/JENKINS-24141
+ * https://issues.jenkins-ci.org/browse/JENKINS-34763
+ */
+
 def call(Map config = [:]) {
     echo "Sending email notification if previous steps failed: ${config}"
     step([$class: "Mailer",
