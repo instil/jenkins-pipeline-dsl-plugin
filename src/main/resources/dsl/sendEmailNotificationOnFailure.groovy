bf4c81730deceb41001b9f1dee770b6b3c17526d
Chris van Es <cvanes@gmail.com>
2016-06-02 17:59:16 +0100

    Sending email for all unstable builds by default

--- src/main/resources/dsl/sendEmailNotificationOnFailure.groovy
+++ src/main/resources/dsl/sendEmailNotificationOnFailure.groovy
@@ -3,7 +3,7 @@ package dsl
 def call(Map config = [:]) {
     echo "Sending email notification if previous steps failed: ${config}"
     step([$class: "Mailer",
-          notifyEveryUnstableBuild: config.notifyForEveryUnstableBuild ?: false,
+          notifyEveryUnstableBuild: config.notifyForEveryUnstableBuild ?: true,
           recipients: config.additionalRecipients ?: "",
           sendToIndividuals: config.notifyCommitters ?: true])
 }
